dbd5154e41b6ef1081ca06f39f9c034103caefa1
Maurice Courtois <mcourtois@vlad-Baar>
2019-11-15 01:13:58 +0100

    - Mainly cleanup... v6.1.0

--- provision.sh
+++ provision.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 sudo yum groups mark convert
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum update -y
 sudo yum groupinstall -y "Development Tools"
-#sudo yum update -y && sudo yum install -y git2u-all curl gcc kernel-devel kernel-headers dkms make bzip2 perl wget net-tools vim rsync bind-utils whois bash-completion*
 sudo yum remove -y  git
 sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum install -y git2u-all curl bzip2 perl wget net-tools vim rsync bind-utils whois bash-completion openssl-devel 
-export KERN_DIR=/usr/src/kernels/`uname -r`
-
-#wget http://download.virtualbox.org/virtualbox/6.0.10/VBoxGuestAdditions_6.0.10.iso
-#sudo mkdir /media/VBoxGuestAdditions
-#sudo mount -o loop,ro VBoxGuestAdditions_6.0.10.iso /media/VBoxGuestAdditions
-# This will fail because the running kernel will not match the installed kernel
-# you need to reboot first to continue with that setup
-#sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-#rm VBoxGuestAdditions_6.0.10.iso*
-#sudo umount /media/VBoxGuestAdditions
-#sudo rmdir /media/VBoxGuestAdditions
+sudo yum install -y git2u-all curl bzip2 perl wget net-tools vim rsync bind-utils whois bash-completion openssl-devel psmisc
 
 # Cleanup
-#echo "==> Clean up yum cache of metadata and packages to save space"
-#sudo yum -y --enablerepo='*' clean all
-#echo "==> Clear core files"
-#sudo rm -f /core*
+echo "==> Clean up yum cache of metadata and packages to save space"
+sudo yum -y --enablerepo='*' clean all
+echo "==> Clear core files"
+sudo rm -f /core*
 
-#echo "==> Removing temporary files used to build box"
-#sudo rm -rf /tmp/*
+echo "==> Removing temporary files used to build box"
+sudo rm -rf /tmp/*
 
