e8f68e8fde8d08285f04aca08a403d81b10f946c
Vlad on iMac in Baar <vlad@Maurices-iMac>
2019-11-19 01:55:54 +0100

    - Add vim yml config

--- provision.sh
+++ provision.sh
@@ -7,6 +7,12 @@ sudo yum remove -y  git
 sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 sudo yum install -y git2u-all curl bzip2 perl wget net-tools vim rsync bind-utils whois bash-completion* openssl-devel psmisc nc
 
+# Special config for vim and yml
+echo '" add yaml stuffs' > /home/vagrant/.vimrc
+echo "au! BufNewFile,BufReadPost *.{yaml,yml} set filetype=yaml foldmethod=indent" >> /home/vagrant/.vimrc
+echo "autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab" >> /home/vagrant/.vimrc
+chown vagrant:vagrant /home/vagrant/.vimrc
+
 # Cleanup
 echo "==> Clean up yum cache of metadata and packages to save space"
 sudo yum -y --enablerepo='*' clean all
